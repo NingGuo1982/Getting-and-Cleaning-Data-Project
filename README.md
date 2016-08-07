f036a3df09e8c3cb5eb7013418d8d76cfdfacb3c
solyrithm <mohamed.fci271@gmail.com>
2016-08-07 16:11:54 +0300

    Third Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+How to reproduce this project
+
+1- Open the R script run_analysis.r using a text editor.
+2- Change the parameter of the directory to the working directory.
+3- Run the R script run_analysis.r.
+
+
+Obtained results:
+
+1- Tidy dataset file tidy_data.txt.
--- run_analysis.R
+++ run_analysis.R
@@ -1,39 +1,45 @@
-train_data <- read.table(file = "C:/Users/Mohamed Hassan/Desktop/UCI HAR Dataset/train/X_train.txt", header = FALSE)
-
-test_data <- read.table(file = "C:/Users/Mohamed Hassan/Desktop/UCI HAR Dataset/test/X_test.txt", header = FALSE)
-
-features <- read.table("C:/Users/Mohamed Hassan/Desktop/UCI HAR Dataset/features.txt", stringsAsFactors=FALSE)
-
-subject <- read.table("C:/Users/Mohamed Hassan/Desktop/UCI HAR Dataset/train/subject_train.txt", stringsAsFactors=FALSE)
-
-train_data <- cbind(train_data, subject)
-
-activity <- read.table("C:/Users/Mohamed Hassan/Desktop/UCI HAR Dataset/train/y_train.txt", stringsAsFactors=FALSE)
-
-train_data <- cbind(train_data, activity)
-
-subject <- read.table("C:/Users/Mohamed Hassan/Desktop/UCI HAR Dataset/test/subject_test.txt", stringsAsFactors=FALSE)
-
-test_data <- cbind(test_data, subject)
-
-activity <- read.table("C:/Users/Mohamed Hassan/Desktop/UCI HAR Dataset/test/y_test.txt", stringsAsFactors=FALSE)
-
-test_data <- cbind(test_data, activity)
-
-all_data <- rbind(train_data, test_data)
-
-features <- rbind(features, "Subject")
-
-features <- rbind(features, "Activity")
-
-names(all_data) <- features[,2]
-
-mean_std <- subset(all_data, select = grep("*mean*|*std*|Subject|Activity", names(all_data)))
-
-all_data$Activity <- as.factor(all_data$Activity)
-
-all_data$Subject <- as.factor(all_data$Subject)
-
-grouped_data <- group_by(all_data, Subject, Activity)
-
-summarise(grouped_data)
+##This R Scipt requires the qdap and reshape2 packages to be installed and loaded
+##Load features to be used as column names for measurements on test and train
+features <- readLines("C:/Users/Mohamed Hassan/Desktop/UCI HAR Dataset/features.txt", stringsAsFactors=FALSE)
+##Load the test and train data with the features as the column names
+test <- read.table(file = "C:/Users/Mohamed Hassan/Desktop/UCI HAR Dataset/test/X_test.txt", header = FALSE)
+train <- read.table(file = "C:/Users/Mohamed Hassan/Desktop/UCI HAR Dataset/train/X_train.txt", header = FALSE)
+##Load the subjects numbers
+testSubject <- read.table("C:/Users/Mohamed Hassan/Desktop/UCI HAR Dataset/test/subject_test.txt", stringsAsFactors=FALSE)
+trainSubject <- read.table("C:/Users/Mohamed Hassan/Desktop/UCI HAR Dataset/test/subject_train.txt", stringsAsFactors=FALSE)
+#Load the activity numbers that we will use to merge the test and train datasets
+testActivity <- read.table("C:/Users/Mohamed Hassan/Desktop/UCI HAR Dataset/test/y_test.txt", stringsAsFactors=FALSE)
+trainActivity <- read.table("C:/Users/Mohamed Hassan/Desktop/UCI HAR Dataset/test/y_train.txt", stringsAsFactors=FALSE)
+##Column bind the subject numbers to their respective data set
+test <- cbind(testSubject, test) ; train <- cbind(trainSubject, train)
+##Column bind the activity numbers to their respective data set
+test <- cbind(testActivity, test) ; train <- cbind(trainActivity, train)
+##Merge the 2 data sets
+mergedData <- merge(test, train, all = T)
+##Subset columns that calculate mean or standard deviation for each column
+##Also subset the activity and subject columns
+mergedData <- mergedData[, grepl("activity|subject|[Mm]ean|[Ss]td",
+                                 names(mergedData))]
+mergedData <- mergedData[, !grepl("angle|meanFreq", names(mergedData))]
+##Substitute the activity numbers with descriptive activity names using mgsub()
+##in dqap package
+activityNumbers <- c("1", "2", "3", "4", "5", "6")
+activityNames <- c("walking", "walkingUpstairs", "walkingDownstairs", "sitting",
+                   "standing","laying")
+if(!require(qdap)) {install.packages("qdap") ; library(qdap)}
+mergedData$activity <- mgsub(activityNumbers, activityNames, 
+                              mergedData$activity)
+##Tidy up variable names by making column names lower case, removing full stops,
+##removing the numbers at the beginning and replacing "t"/"f" with "time"/"freq"
+colnames(mergedData) <- tolower(colnames(mergedData))
+colnames(mergedData) <- gsub(".", "", colnames(mergedData), fixed = T)
+colnames(mergedData) <- gsub("^x[0-9]*[0-9]", "", colnames(mergedData))
+colnames(mergedData) <- gsub("^t", "time", colnames(mergedData))
+colnames(mergedData) <- gsub("^f", "freq", colnames(mergedData))
+##Using the reshape2 package, melt the data set and then cast the melted data
+##set to summarise the averages of each variable
+if(!require(reshape2)) {install.packages("reshape2") ; library(reshape2)}
+mergedDataMelt <- melt(mergedData, (id = c("subject", "activity")))
+tidyData <- dcast(mergedDataMelt, subject + activity ~ variable, mean)
+##Write the tidy data set as a text file in the working directory
+write.table(tidyData, file = "tidydata.txt", row.names = F)
